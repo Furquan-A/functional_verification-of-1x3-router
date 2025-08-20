e0fac7b2793b45c8604d8af3c2670e611955fc32
Furquan-A <furquan.741@gmail.com>
2025-08-19 23:43:08 -0700

    changes in the driver class

--- Router Project/wr_agt_top/router_wr_driver.sv
+++ Router Project/wr_agt_top/router_wr_driver.sv
@@ -1,5 +1,6 @@
 class router_wr_driver extends uvm_driver #(router_wr_xtns);
 `uvm_component_utils(router_wr_driver)
+wr_agent_config m_cfg;
 
 virtual router_if vif;
 function new (string name = "router_wr_driver", uvm_component parent);
@@ -23,7 +24,9 @@ endtask
 
 task send_to_dut(router_wr_xtns xtn);
 // based on the waveform of the protocol
-wait(vif.wdr_cb.busy)
+uvm_info("ROUTER_WR_DRIVER","$sformatf"("printing from driver \n ", xtn.sprint()),UVM_LOW)
+@(vif.wdr_cb);
+wait(!vif.wdr_cb.busy)
 vif.wdr_cb.pkt_valid <= 1;
 vif.wdr_cb.data_in <= xtn.header;
 @(vif.wdr_cb)
@@ -31,7 +34,13 @@ foreach (xtn.payload_data[i])
 begin 
 wait(!vif.wdr_cb.busy)
 vif.wdr_cb.data_in <= xtn.payload_data[i];
+@(vif.wdr_cb);
+end 
 
+wait(!vif.wdr_cb.busy)
 vif.wdr_cb.pkt_valid <= 0;
 vif.wdr_cb.data_in <= xtn.parity;
+repeat(2)@(vif.wdr_cb);
+m_cfg.drv_data_count ++;
 endtask
+endclass
--- Router Project/wr_agt_top/router_write_monitor.sv
+++ Router Project/wr_agt_top/router_write_monitor.sv
@@ -1,58 +1,33 @@
 class router_wr_monitor extends uvm_monitor;
 `uvm_component_utils(router_wr_monitor)
 
-//analysis port for the monitor 
-uvm_analysis_port #(router_wr_xtns) ap;
 virtual router_if.WMON_MP vif;
-wr_agent_config w_cfg;
-router_wr_xtns xtns;
+uvm_analysis_port #(router_wr_xtns) ap;
+wr_agent_config m_cfg;
+
 
 extern function new(string name = "router_wr_monitor", uvm_component parent);
-extern function void build_phase(build_phase phase);
-extern function void connect_phase(uvm_phase phase);
-extern task run_phase(uvm phase);
+extern function void build_phase(uvm_phase phase);
+extern function connect_phase(uvm_phase phase);
+extern task run_phase(uvm_phase);
 extern task collect_data;
 
 endclass
 
-
-function router_wr_monitor :: new(string name = "router_wr_monitor", uvm_component parent);
+function router_wr_monitor :: new (string name = "router_wr_monitor" uvm_component parent);
 super.new(name,parent);
-ap  = new("ap",this);
-endfunction
-
-function void router_wr_monitor :: build_phase ( uvm_phase phase);
-super.build_phase(phase);
-if(`uvm_config_db #(wr_agent_config)::get(this,"","wr_agent_config",w_cfg))
-	`uvm_fatal("MON_CFG","cannot get the config data")
-endfunction
-
-function void router_wr_monitor :: connect_phase(phase);
-super.connect_phase(phase);
-vif = w_cfg.vif;
 endfunction 
 
-task router_wr_monitor :: run_phase(uvm_phase);
-forever 
-collect_data;
-endtask
+function void router_wr_monitor :: build_phase(uvm_phase phase);
+super.build_phase(phase);
+ap = new("ap");
+endfunction
 
-task router_wr_monitor :: collect_data;
-wait(vif.wmon_cb.pkt_valid)
-xtn = router_wr_xtns :: type_id::create("xtn");
-xtn.header = vif.wmon_cb.data_in;
-xtn.payload_data = new[xtn.header[7:2]];
-@(vif.wmon_cb);
-for(int i=0;i<xtn.header[7:2];i++)
-		begin
-				wait(~vif.w_mon_cb.busy)
-				xtn.payload_data[i]=vif.w_mon_cb.data_in;
-				@(vif.w_mon_cb);
-		end
-	wait(~vif.w_mon_cb.busy)
-	xtn.parity=vif.w_mon_cb.data_in;
-	xtn.print;
-	@(vif.w_mon_cb);
-	ap_w.write(xtn);
-endtask
-	
+
+function void router_wr_monitor :: connect_phase(uvm_phase phase);
+super.connect_phase(phase);
+vif = m_cfg.vif;
+endfunction
+
+task run_phase(uvm_phase);
+super.run_phase(phase);
