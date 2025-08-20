8255d8d8dce6ea6668aa99dd5a2d86683025cbb5
Furquan-A <furquan.741@gmail.com>
2025-08-19 23:55:02 -0700

    wr_monitor added

--- Router Project/wr_agt_top/router_write_monitor.sv
+++ Router Project/wr_agt_top/router_write_monitor.sv
@@ -1,33 +1,37 @@
 class router_wr_monitor extends uvm_monitor;
 `uvm_component_utils(router_wr_monitor)
-
-virtual router_if.WMON_MP vif;
 uvm_analysis_port #(router_wr_xtns) ap;
 wr_agent_config m_cfg;
-
+router_wr_xtns mon_data;
+virtual router_if vif;
 
 extern function new(string name = "router_wr_monitor", uvm_component parent);
-extern function void build_phase(uvm_phase phase);
-extern function connect_phase(uvm_phase phase);
+extern function build_phase(uvm_phase phase );
+extern function connect_phase (uvm_phase phase);
 extern task run_phase(uvm_phase);
 extern task collect_data;
 
-endclass
+endclass 
 
-function router_wr_monitor :: new (string name = "router_wr_monitor" uvm_component parent);
-super.new(name,parent);
+function router_wr_monitor :: new (string name = "router_wr_monitor", uvm_component parent );
+super.new(name, parent );
 endfunction 
 
 function void router_wr_monitor :: build_phase(uvm_phase phase);
 super.build_phase(phase);
-ap = new("ap");
+ap = new("ap",this);
 endfunction
-
 
 function void router_wr_monitor :: connect_phase(uvm_phase phase);
 super.connect_phase(phase);
 vif = m_cfg.vif;
 endfunction
 
-task run_phase(uvm_phase);
-super.run_phase(phase);
+task router_wr_monitor :: run_phase(uvm_phase);
+forever 
+	collect_data;
+endtask
+
+task router_wr_monitor :: collect_data;
+
+endtask
