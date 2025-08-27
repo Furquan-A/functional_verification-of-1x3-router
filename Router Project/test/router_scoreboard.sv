32b473a4507283e0922e73946436103bdda3e539
Furquan-A <furquan.741@gmail.com>
2025-08-26 21:37:37 -0700

    scoreboard

--- Router Project/test/router_scoreboard.sv
+++ Router Project/test/router_scoreboard.sv
@@ -9,6 +9,28 @@ router_wr_xtns wr_data;
 router_rd_xtns read_cov_data;
 router_wr_xtns write_cov_data;
 
+router_env_config e_cfg;
+int data_verified_count;
+
+covergroup router_fcov;
+	option.per_instance =1;
+	ADDRESS : coverpoint write_cov_data.header[1:0] {
+		bins low = {2'b00};
+		bins mid1 = {2'b01};
+		bins mid2 = {2'b10};
+			}
+
+	PAYLOAD_SIZE : coverpoint write_cov_data.header[7:2] {
+		bins small_packets = {[1:13]};
+		bins midium_packets = {[14:30]};
+		bins large_packets = {[31:63]};
+		}
+	ADDRESS_X_PAYLOAD_SIZE: cross ADDRESS, PAYLOAD_SIZE;
+endgroup
+
+router_fcov router_fcov1;
+router_fcov router_fcov2;
+
 function new( string name = "router_scoreboard", uvm_component parent);
 super.new(name,parent);
 endfunction
@@ -17,12 +39,13 @@ function void build_phase(uvm_phase phase);
 super.build_phase(phase);
 if(!uvm_config_db #(router_env_config) ::get("this","","router_env_config",env_cfg))
 `uvm_fatal("CONFIG","cannot get() the config")
-
+router_fcov1 = new;
+router_fcov2 = new;
 fifo_wrh = new("fifo_wrh",this);
 fifo_rdh = new[env_cfg.no_of_read_agents];// we have to know how many read monitors are present in the design 
 foreach(fifo_rdh[i])
 	begin 
-		fifo_rdh = new($sformatf("fifo_rdh[%0d]",i),this); // It is how we crate the handles for the dunamic types 
+		fifo_rdh[i] = new($sformatf("fifo_rdh[%0d]",i),this); // It is how we crate the handles for the dunamic types 
 	end 
 endfunction 
 
@@ -93,3 +116,13 @@ function void check_data(router_rd_xtns rd);
 	
 	data_verified_count++;
 endfunction
+
+function void report_phase(uvm_phase phase);
+  super.report_phase(phase);
+  $display("==== Write Port Coverage ====");
+  router_fcov1.print();
+  $display("==== Read Port Coverage ====");
+  router_fcov2_inst.print();
+endfunction
+
+endclass
